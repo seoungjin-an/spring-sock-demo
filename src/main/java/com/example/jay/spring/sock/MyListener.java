6ceae94e3086c78286692eba08e9e6e1adac8f73
안성진 <jay@ivycomtech.com>
2021-04-17 10:02:54 +0900

    소켓로그 정리

--- src/main/java/com/example/jay/spring/sock/MyListener.java
+++ src/main/java/com/example/jay/spring/sock/MyListener.java
@@ -5,6 +5,7 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -26,8 +27,10 @@ public class MyListener {
             while (true) {
                 Socket srcSock = sock.accept();
                 Socket dstSock = new Socket(appSettings.targetHost, appSettings.targetPort);
-                executor.execute(() -> sockLoop(srcSock, dstSock));
-                executor.execute(() -> sockLoop(dstSock, srcSock));
+
+                log.info("listenLoop >> new client ----- ");
+                executor.execute(() -> sockLoop("####### Brower request" , srcSock, dstSock));
+                executor.execute(() -> sockLoop("@@@@@@@ Server response", dstSock, srcSock));
             }
         }
         catch (Exception ex) {
@@ -35,16 +38,17 @@ public class MyListener {
         }
     }
 
-    void sockLoop(Socket sock, Socket wSock) {
+    void sockLoop(String label, Socket sock, Socket wSock) {
         try (InputStream s = sock.getInputStream()) {
             OutputStream os = wSock.getOutputStream();
             int recvLen = 0;
             byte[] b = new byte[4096];
             while ((recvLen = s.read(b)) > 0) {
-                log.info(getHexDump(b, recvLen, 30));
                 os.write(b, 0, recvLen);
-                String str = new String(b, 0, recvLen, Charset.forName("UTF-8"));
-                log.info("sockLoop >> " + str);
+                String str = new String(b, 0, recvLen, StandardCharsets.UTF_8);
+                log.info("sockLoop >> {}\n{}", label, str);
+                log.info(getHexDump(b, recvLen, 30));
+                log.info("-------------------------------------------------");
             }
         }
         catch (Exception ex) {
@@ -75,7 +79,7 @@ public class MyListener {
 				line1.append(String.format("%02X", src[i2 + 3] & 0xFF));
 				line1.append(' ');
 			}
-			lines.append(String.format("%3d ", (i/16))).append(line1).append(new String(src, i, 16)).append('\n');
+			lines.append(String.format("%3d ", (i/16))).append(line1).append(utf8byte2str(src, i, 16)).append('\n');
 			pos = i + 16;
 			if (maxLine > 0 && lineCount == maxLine)
 				break;
@@ -90,9 +94,59 @@ public class MyListener {
 				if ((i%4) == 3)
 					line1.append(' ');
 			}
-			lines.append(String.format("%3d ", (pos/16))).append(line1).append(new String(src, pos, len - pos)).append('\n');
+			lines.append(String.format("%3d ", (pos/16))).append(line1).append(utf8byte2str(src, pos, len - pos)).append('\n');
 		}
 		
 		return lines.toString();
 	}
+
+    // utf-8: byte -> String
+    private static String utf8byte2str(byte[] bytes, int pos, int len) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; ) {
+            int b = (bytes[pos + i] & 0x000000FF);
+            if ((b & 0x80) == 0) {
+                sb.append(0x20 <= b && b <= 0x7e ? Character.toString(b) : ".");
+                i++;
+            }
+            // 110x xxxx, 10xx xxxx 패턴 
+            else if ((b & 0xE0) == 0xC0) {
+                if (i + 1 < len) {
+                    int b2 = (bytes[pos + i + 1] & 0x000000FF);
+                    sb.append((b2 & 0xC0) == 0x80 ? new String(bytes, pos + i, 2, StandardCharsets.UTF_8) : "??");
+                    i += 2;
+                }
+                else {
+                    sb.append(Character.toString(b));
+                    i = len;
+                }
+            }
+            // 1110 xxxx, 10xx xxxx, 10xx xxxx 패턴
+            else if ((b & 0xF0) == 0xE0) {
+                if (i + 2 < len) {
+                    int b2 = (bytes[pos + i + 1] & 0x000000FF);
+                    int b3 = (bytes[pos + i + 1] & 0x000000FF);
+                    sb.append((b2 & 0xC0) == 0x80 && (b3 & 0xC0) == 0x80 ? new String(bytes, pos + i, 3, StandardCharsets.UTF_8) : "???");
+                    i += 3;
+                }
+                else {
+                    sb.append("??".substring(0, len - i));
+                    i = len;
+                }
+            }
+            // 1111 0zzz, 10zz xxxx, 10xx xxxx, 10xx xxxx 패턴
+            else {
+                if (i + 3 < len) {
+                    sb.append("????");
+                    i += 4;
+                }
+                else {
+                    sb.append("???".substring(0, len - i));
+                    i = len;
+                }
+            }
+        }
+
+        return sb.toString();
+    }
 }
